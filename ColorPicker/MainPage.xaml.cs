f194342b43b2a6d26bc889e537f64445d297f630
Mohammodullah Emran <63514104+emranmho@users.noreply.github.com>
2025-03-13 14:48:44 +0600

    Add project files.

---
+++ ColorPicker/MainPage.xaml.cs
@@ -0,0 +1,54 @@
+﻿namespace ColorPicker
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+            this.Loaded += MainPage_Loaded;
+        }
+
+        private void MainPage_Loaded(object? sender, EventArgs e)
+        {
+            SetBackgroundColor(Color.FromRgb(0, 0, 0));
+        }
+
+        private void SetBackgroundColor(Color color)
+        {
+            LblInfo.Text = "";
+            AppBackground.BackgroundColor = color;
+            RgbValue.Text = color.ToHex();
+        }
+
+        private void slrValue_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            var red = RValue.Value;
+            var green = GValue.Value;
+            var blue = BValue.Value;
+
+            SetBackgroundColor(Color.FromRgb(red, green, blue));
+        }
+
+        private void GenerateRandomColor(object sender, EventArgs e)
+        {
+            var random = new Random();
+
+            var color = Color.FromRgb(
+                random.Next(0, 255),
+                random.Next(0, 255),
+                random.Next(0, 255));
+
+            SetBackgroundColor(color);
+
+        }
+
+        private async void CopyBtn_Clicked(object? sender, EventArgs e)
+        {
+            await Clipboard.SetTextAsync(RgbValue.Text);
+            LblInfo.Text = "Copied to clipboard";
+        }
+    }
+
+}
---
+++ TipCalculator/MainPage.xaml.cs
@@ -0,0 +1,64 @@
+﻿namespace TipCalculator
+{
+    public partial class MainPage : ContentPage
+    {
+        private double billAmount = 0.00d;
+        private int tip = 0;
+        private double tipAmount = 0.00d;
+        private int split = 1;
+        private double perPersonAmount = 0.00d;
+
+        public MainPage()
+        {
+            InitializeComponent();
+            UpdateDisplay();
+        }
+
+        private void UpdateDisplay()
+        {
+            LblTip.Text = tip.ToString();
+            LblTipAmount.Text = tipAmount.ToString("F2");
+            LblSplit.Text = split.ToString();
+
+            LblSplitAmount.Text = perPersonAmount.ToString("F2");
+        }
+
+        private void TbTotal_Completed(object sender, EventArgs e)
+        {
+            billAmount = double.Parse(TbTotal.Text);
+
+            TbTotal.Unfocus();
+            SlrTip.Focus();
+            Calculate();
+        }
+
+        private void Calculate()
+        {
+            tipAmount = billAmount * tip / 100;
+            perPersonAmount = (billAmount + tipAmount) / split;
+            UpdateDisplay();
+        }
+
+        private void SlrTip_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            tip = (int)SlrTip.Value;
+            Calculate();
+        }
+
+        private void BtnMinus_Clicked(object sender, EventArgs e)
+        {
+            if (split > 1)
+            {
+                split--;
+            }
+            Calculate();
+        }
+
+        private void BtnPlus_Clicked(object? sender, EventArgs e)
+        {
+            split++;
+            Calculate();
+        }
+    }
+
+}
